dbb7e41239d951cb688e7bb5f486c1146c43f30c
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-04-14 16:32:57 +0530

    add: main section finished

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const pass = document.querySelector("#pass");
+const cpass = document.querySelector("#cpass");
 
+cpass.addEventListener("input", () => {
+  if (pass.value != cpass.value) {
+    cpass.setCustomValidity("Passwords doesn't match");
+  } else {
+    cpass.setCustomValidity("");
+  }
+});
+
+document.querySelector("button").addEventListener("submit", (e) => {
+  if (!cpass.checkValidity()) {
+    e.preventDefault();
+  }
+});
